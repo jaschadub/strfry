96100847c65b0aef95eba5373d58043b2352ed83
Doug Hoyte <doug@hcsw.org>
2023-06-14 23:48:14 -0400

    wip

--- src/apps/web/WebReader.cpp
+++ src/apps/web/WebReader.cpp
@@ -6,6 +6,29 @@
 
 
 
+std::string renderTimestamp(uint64_t now, uint64_t ts) {
+    uint64_t delta = now > ts ? now - ts : ts - now;
+
+    const uint64_t A = 60;
+    const uint64_t B = A*60;
+    const uint64_t C = B*24;
+    const uint64_t D = C*30.5;
+    const uint64_t E = D*12;
+
+    std::string output;
+
+    if      (delta < B) output += std::to_string(delta / A) + " minutes";
+    else if (delta < C) output += std::to_string(delta / B) + " hours";
+    else if (delta < D) output += std::to_string(delta / C) + " days";
+    else if (delta < E) output += std::to_string(delta / D) + " months";
+    else                output += std::to_string(delta / E) + " years";
+
+    if (now > ts) output += " ago";
+    else output += " in the future";
+
+    return output;
+}
+
 std::string getParentEvent(const tao::json::value &json) {
     std::string parent;
 
@@ -148,6 +171,8 @@ struct EventThread {
     TemplarResult render(lmdb::txn &txn, Decompressor &decomp) {
         if (!found) return TemplarResult{ "event not found" };
 
+        auto now = hoytech::curr_time_s();
+
         std::function<TemplarResult(const std::string &)> process = [&](const std::string &id){
             auto p = eventCache.find(id);
             if (p == eventCache.end()) throw herr("unknown id");
@@ -155,12 +180,14 @@ struct EventThread {
 
             struct {
                 std::string comment;
+                std::string timestamp;
                 const Event *ev;
                 const User *user;
                 std::vector<TemplarResult> replies;
             } ctx;
 
             ctx.comment = elem.json.at("content").get_string();
+            ctx.timestamp = renderTimestamp(now, elem.json.at("created_at").get_unsigned());
             ctx.user = userCache.getUser(txn, decomp, std::string(sv(elem.ev.flat_nested()->pubkey())));
             ctx.ev = &elem;
 
