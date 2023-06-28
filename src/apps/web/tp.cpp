5af221eb15fc8983a2a1ddd73825081d33a6e451
Doug Hoyte <doug@hcsw.org>
2023-06-27 23:32:30 -0400

    wip

--- src/apps/web/tp.cpp
+++ src/apps/web/tp.cpp
@@ -14,26 +14,45 @@ namespace algo {
     struct comment :
         pegtl::seq<
             pegtl::one< '#' >,
-            tao::pegtl::until< tao::pegtl::eolf >
+            pegtl::until< pegtl::eolf >
         > {};
 
     struct ws : pegtl::sor< pegtl::space, comment > {};
 
     template< typename R >
-    struct pad : tao::pegtl::pad< R, ws > {};
+    struct pad : pegtl::pad< R, ws > {};
 
 
     // Pubkeys
 
-    struct pubkey :
+    struct npub :
         pegtl::seq<
             pegtl::string< 'n', 'p', 'u', 'b', '1' >,
             pegtl::plus< pegtl::alnum >
         > {};
 
-    struct pubkeySetOp : tao::pegtl::one< '+', '-' > {};
+    struct pubkey :
+        pegtl::sor<
+            npub,
+            pegtl::identifier
+        > {};
 
-    struct pubkeyList : pegtl::list< pubkey, pubkeySetOp, ws > {};
+    struct pubkeySetOp : pegtl::one< '+', '-' > {};
+
+    struct pubkeyGroup;
+    struct pubkeyList : pegtl::list< pubkeyGroup, pubkeySetOp, ws > {};
+
+    struct pubkeyGroupOpen : pegtl::one< '(' > {};
+    struct pubkeyGroupClose : pegtl::one< ')' > {};
+
+    struct pubkeyGroup : pegtl::sor<
+        pubkey,
+        pegtl::seq<
+            pad< pubkeyGroupOpen >,
+            pubkeyList,
+            pad< pubkeyGroupClose >
+        >
+    > {};
 
 
 
@@ -41,19 +60,90 @@ namespace algo {
 
     struct letIdentifier : pegtl::identifier {};
 
+    struct variableIdentifier : pegtl::seq< pegtl::not_at< npub >, pegtl::identifier > {};
+
     struct let :
         pegtl::seq<
             pad< pegtl::string< 'l', 'e', 't' > >,
-            pad< letIdentifier >,
+            pad< variableIdentifier >,
             pad< pegtl::one< '=' > >,
             pad< pubkeyList >,
             pegtl::one< ';' >
         > {};
 
 
+
+
+    // Posts block
+
+    struct number :
+        pegtl::if_then_else< pegtl::one< '.' >,
+            pegtl::plus< pegtl::digit >,
+            pegtl::seq<
+                pegtl::plus< pegtl::digit >,
+                pegtl::opt< pegtl::one< '.' >, pegtl::star< pegtl::digit > >
+            >
+        > {};
+
+    struct arith :
+        pegtl::seq<
+            pad< pegtl::one< '+', '-' > >,
+            number
+        > {};
+
+    struct regexp :
+        pegtl::seq<
+            pegtl::one< '/' >,
+            pegtl::star< pegtl::sor< pegtl::string< '\\', '/' >, pegtl::not_one< '/' > > >,
+            pegtl::one< '/' >,
+            pegtl::star< pegtl::alpha >
+        > {};
+
+    struct likedByCondition :
+        pegtl::seq<
+            pad< pegtl::string< 'l', 'i', 'k', 'e', 'd' > >,
+            pad< pegtl::string< 'b', 'y' > >,
+            pad< variableIdentifier >
+        > {};
+
+    struct contentCondition :
+        pegtl::seq<
+            pad< pegtl::string< 'c', 'o', 'n', 't', 'e', 'n', 't' > >,
+            pad< pegtl::one< '~' > >,
+            pad< regexp >
+        > {};
+
+    struct condition :
+        pegtl::sor<
+            pad< likedByCondition >,
+            pad< contentCondition >
+        > {};
+
+    struct filterStatment :
+        pegtl::seq<
+            pegtl::sor<
+                pad< pegtl::string< 'd', 'r', 'o', 'p' > >,
+                pad< arith >
+            >,
+            pad< pegtl::string< 'i', 'f' > >,
+            pad< condition >,
+            pegtl::one< ';' >
+        > {};
+
+    struct postBlock :
+        pegtl::seq<
+            pad< pegtl::string< 'p', 'o', 's', 't' > >,
+            pad< pegtl::one< '{' > >,
+            pegtl::star< pad< filterStatment > >,
+            pegtl::one< '}' >
+        > {};
+
+
+
+
     // Main
 
-    struct anything : pegtl::sor< ws, let > {};
+    struct anything : pegtl::sor< ws, let, postBlock > {};
     struct main : pegtl::until< pegtl::eof, pegtl::must< anything > > {};
 
 
@@ -78,6 +168,22 @@ namespace algo {
     };
 
     template<>
+    struct action< pubkeyGroupOpen > {
+        template< typename ActionInput >
+        static void apply( const ActionInput& in, std::string& v ) {
+            std::cout << "PKGRPOPEN: " << in.string() << std::endl;
+        }
+    };
+
+    template<>
+    struct action< pubkeyGroupClose > {
+        template< typename ActionInput >
+        static void apply( const ActionInput& in, std::string& v ) {
+            std::cout << "PKGRPCLOSE: " << in.string() << std::endl;
+        }
+    };
+
+    template<>
     struct action< pubkeyList > {
         template< typename ActionInput >
         static void apply( const ActionInput& in, std::string& v ) {
@@ -90,6 +196,14 @@ namespace algo {
         template< typename ActionInput >
         static void apply( const ActionInput& in, std::string& v ) {
             std::cout << "PKOP: " << in.string() << std::endl;
+        }
+    };
+
+    template<>
+    struct action< regexp > {
+        template< typename ActionInput >
+        static void apply( const ActionInput& in, std::string& v ) {
+            std::cout << "RE: " << in.string() << std::endl;
         }
     };
 }
